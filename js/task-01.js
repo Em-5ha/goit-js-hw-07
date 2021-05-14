77e63ef0b1f84c3c87210a9f194da5b4b610ba2c
Em-5ha <masha.nemo@gmail.com>
2021-05-14 17:10:13 +0300

    HW 7 start

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const ulRef = document.querySelector('#categories');
+const itemRef = document.querySelectorAll('.item .ul');
+console.log(`В списке ${ulRef.children.length} категории.`);
+// console.log(itemRef.firstElementChild.textContent);
+console.log(ulRef.children);
+console.log(itemRef);
